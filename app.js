4cecf5502482ff5374e63c6d18091e5e0229cbbb
TalentoP <julianalvarez1282@yahoo.es>
2025-03-16 14:11:22 -0500

    Agregada funcionalidad de agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+
+    let amigo = document.querySelector('#amigo');
+
+    if (amigo.value.trim() === '') {
+        alert("Por favor ingrese un nombre");
+    } else {
+        amigos.push(amigo.value);
+    }
+    amigo.value = '';
+
+}
